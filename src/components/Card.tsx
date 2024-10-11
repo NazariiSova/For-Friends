9050e3f4cb8dc84f20bd84aca60cb77c7b681fcd
NazariiSova <madrigalamar@gmail.com>
2024-10-11 14:13:02 +0300

    fix card with hardcode data

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,38 +1,30 @@
-'use client';
-
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Gear1Img from '../img/gear1.jpg';  
-import { useStore } from '../store/useStore';
-import { GearCard, TripCard } from '../store/useStore'; 
+import { GearCard, TripCard } from '../store/useStore';
 
 import '../styles/card.scss';
 
 type CardProps = {
-  card: (GearCard | TripCard)[];
+  card: GearCard | TripCard; 
 };
 
-export default function Card() {
-  const { gearCards, tripCards } = useStore(); 
-  const card = gearCards[0]; 
-
-
-  if (!card) return <p>No card found</p>; 
+export default function Card({ card }: CardProps) {
+  if (!card) return <p>No card found</p>;
 
   return (
     <div className="card">
       <Image
-        src={card?.imageUrl}  
-        alt={card?.title }
+        src={card.imageUrl}
+        alt={card.title}
         className="card-image"
       />
-      <h3>{card?.title}</h3>
-      <p>{card?.date}</p>
+      <h3>{card.title}</h3>
+      <p>{card.date}</p>
       <div className="card-hover">
-        <p>{card?.fullContent}</p>
+        <p>{card.fullContent}</p>
       </div>
-      <Link href={`/${card?.type === 'gear' ? 'gears' : 'trips'}/${card?.id}`}>
+      <Link href={`/${card.type === 'gear' ? 'gears' : 'trips'}/${card.id}`}>
         Read More
       </Link>
     </div>
